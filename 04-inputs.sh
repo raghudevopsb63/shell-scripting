60781e1f2aede77a4a4e9a6592453354b25f5c99
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-10 07:07:10 +0530

    Input Special Variables

--- 04-inputs.sh
+++ 04-inputs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p 'Enter your Name: ' name
+read -p "Enter your Name: " name
 echo "Your Name = $name"
 
 # Special Variables
---
+++ 05-quotes.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo Apples Cost is $100
+
