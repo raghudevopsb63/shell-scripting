082b66b851d259f919676f185bb605c8313613f7
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-03-16 07:26:39 +0530

    Create EC2 Instance Script

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,17 +6,20 @@ if [ -z "$1" ]; then
 fi
 
 COMPONENT=$1
+ZONE_ID="Z08005452NVNO9YZAOL1R"
 
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 echo $AMI_ID
-aws ec2 run-instances \
+PRIVATE_IP=$(aws ec2 run-instances \
     --image-id ${AMI_ID} \
     --instance-type t2.micro \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
     --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
     --security-group-ids ${SGID} \
-    | jq
+    | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
